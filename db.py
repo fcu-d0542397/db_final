5a4d057658c1ab927c25eebb830051c88748a06a
ak47969 <ak47969@gmail.com>
2020-06-22 10:00:52 +0800

    call function

--- db.py
+++ db.py
@@ -1,20 +1,26 @@
 import csv
+from search import searchStuID
+code = input('請輸入功能：')
 list_test = []
-with open('DB_students.csv',newline='') as csvfile:
-    rows = csv.reader(csvfile)
-    for row in rows:
-        list_test.append([row[0],row[1]])
-    list_test = list_test[1:-1]
-    list_test.sort()
-    file = open('order.csv','w+') 
-    for i in range(len(list_test)):
-        file.write(list_test[i][0])
-        file.write(',')
-        file.write(list_test[i][1])
-        file.write(',')
-        if i==len(list_test)-1:
-            file.writelines("End")
-        else:
-            file.write(str(i+2))
-            file.writelines("\n")
-    
+count = 0
+size = 0
+if code == '1':
+    with open('DB_students.csv',newline='') as csvfile:
+        rows = csv.reader(csvfile)
+        for row in rows:
+            list_test.append([row[0],row[1]])
+        list_test = list_test[1:-1]
+        list_test.sort()
+        file = open('order.csv','w+') 
+        for i in range(len(list_test)):
+            file.write(list_test[i][0])
+            file.write(',')
+            file.write(list_test[i][1])
+            file.write(',')
+            if i==len(list_test)-1:
+                file.writelines("End")
+            else:
+                file.write(str(i+2))
+                file.writelines("\n")
+elif code == '2':
+    print(searchStuID(count,size))
--- search.py
+++ search.py
@@ -1,39 +1,33 @@
 import linecache
-
-# def get_line_context(file_path, line_number):
-#         return linecache.getline(file_path, line_number).strip()
-
-# print (get_line_context('./order.csv',86754))
-# file = open('order.csv','r')
-ret = []
 number = []
 studentid = 'D000006491'
 search = 0
 count = 0
 def read(number):
         file = open('order.csv','r')
-        for line in file.readlines()[number:number+10]:
+        ret = []
+        for line in file.readlines()[number:number+100]:
                 # print('ret[i]')
                 temp=line.rstrip('\n')
                 temp = temp.split(',')
                 ret.append(temp)
         file.close()
+        return ret
+def searchStuID(count,size):
+        ret = []
+        number = []
+        while count < size:
+                ret=read(count)
+                # print(ret)
+                count = count+100
+                for i in range(len(ret)):
+                        search = ret[i][0]
+                        if studentid == search:
+                                number.append(ret[i])
+                                # print(ret[i])
+                ret=[]
+        print(number)
+        return number
+        
+# print(searchStuID(count,3000))
 
-# for i in range(50):
-#         file = open('order.csv','r')
-#         for line in file.readlines()[i:i+10]:
-#                 # print('ret[i]')
-#                 temp=line.rstrip('\n')
-#                 temp = temp.split(',')
-#                 ret.append(temp)
-#         file.close()
-
-while count < 3000:
-        read(count)
-        count = count+10
-        for i in range(len(ret)):
-                search = ret[i][0]
-                if studentid == search:
-                        number.append(ret[i])
-        ret=[]
-print(number)
